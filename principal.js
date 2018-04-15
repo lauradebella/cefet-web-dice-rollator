7b2085aa08da3989f53f7581b9b38d38be2ae765
lbella <lbella@thoughtworks.com>
2018-04-15 16:40:38 -0300

    Exercise done

--- principal.js
+++ principal.js
@@ -1,6 +1,3 @@
-// Faça seu exercício neste arquivo
-
-
 // Alguns elementos importantes na página (index.html):
 // #rolar: Botão rolar
 //	 - você deve atribuir um handler de evento a ele para fazer o cálculo da rolagem dos dados
@@ -11,4 +8,26 @@
 // .quantidade: todos os input[type=number] com a quantidade de dados a serem rolados
 // #quantidadeD{4,6,8,10,12,20}: um ID para cada input[type=number] com a quantidade
 
+var diceMovement = function(valor, quantidade) {
+  var soma = 0;
+  for(var index = 0; index < quantidade; index++){
+    soma += Math.ceil(Math.random() * valor);
+  }
+  return soma;
+}
 
+document.getElementById('rolar').addEventListener('click', function(){
+  var dices = [4,6,8,10,12,20],
+      sum = 0,
+      values = [];
+  dices.forEach(function(val){
+    var id = 'quantidadeD'+val;
+    var qnt = document.getElementById(id).value;
+    var result = diceMovement(val, qnt);
+    sum += result;
+    values.push(result);
+	});
+
+  document.getElementById('recipienteResultados').classList.remove('oculto');
+  document.getElementById('resultado').innerHTML = values + "\n soma = " + sum;
+});
